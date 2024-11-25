e64a2008d748d7bc5b8ac14db11d247a0d0667c1
Cami Wang <iris0952@gmail.com>
2024-11-25 20:22:32 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
-<script lang="ts" setup></script>
+<script setup></script>
 
 <template>
   <div>
-    Page: 首頁
+    <h1>首頁</h1>
+    <h3>測試字體</h3>
+    <p class="title">標題</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$primary: #426bb8;
+h1{
+  color: $primary;
+}
+button.btn{
+  cursor: pointer;
+  margin-right: 10px;
+}
+</style>
